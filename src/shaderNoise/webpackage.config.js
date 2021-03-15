f12fd65059fce26b4a2a75b8f4aa4cf057125221
evayann (Yann Zavattero) <yann.zavattero@hotmail.com>
2021-03-15 10:39:34 +0100

    Shader Noise

---
+++ src/shaderNoise/webpackage.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+let webpack = require('webpack');
+
+let ignore = new webpack.IgnorePlugin(new RegExp("/(node_modules|ckeditor)/"))
+
+module.exports = {
+    mode: "development",
+    entry: "./src/shaderNoise/shaderNoise.ts",
+    output: {
+        path: path.join(__dirname, "../../dist/shaderNoise"),
+        filename: "shaderNoiseBundle.js"
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /(node_modules)/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    plugins: [
+        ignore
+    ]
+}
